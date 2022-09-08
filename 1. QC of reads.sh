6efd1d2404d5b5979e1fe972d032662019202836
Alison Díaz <68522468+alisondiazc@users.noreply.github.com>
2022-09-08 12:28:26 -0500

    Update 1. QC of reads.sh

--- 1. QC of reads.sh
+++ 1. QC of reads.sh
@@ -9,17 +9,27 @@ cp /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYH007/Cell-3/m54336U_220420_105
 cat *.fastq.gz > PYM007_reads.fastq.gz 
 
 #Crear carpetas para QC
-cd ..
 mkdir 1.ReadsQC
-cd 1.ReadsQC 
-mkdir LongQC
-mkdir FastQC 
+cd 1.ReadsQC  
 
 #Análisis con LongQC
+mkdir LongQC
 cd LongQC
 ln -s /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/reads/PYM007_reads.fastq.gz . 
 module load anaconda3/2021.05
 module load longqc/1.2.0
 python /cm/shared/apps/longqc/LongQC-1.2.0/longQC.py sampleqc -x pb-hifi -s PYM007 -p 80 -o LongQC PYM007_reads.fastq.gz
 
+#Análisis con FastQC 
+mkdir FastQC
+cd FastQC 
+ln -s /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/reads/PYM007_reads.fastq.gz .
+module load fastqc/0.11.3
+fastqc PYM007_reads.fastq.gz 
 
+#Limpieza y filtrado de reads 
+mkdir 2.ReadsTrimming
+cd 2.ReadsTrimming
+ln -s /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/reads/PYM007_reads.fastq.gz .
+module load hifiadapterfilt/2.0.0
+pbadapterfilt.sh -p ./ -l 44 -m 97 -t 60 -o . 
