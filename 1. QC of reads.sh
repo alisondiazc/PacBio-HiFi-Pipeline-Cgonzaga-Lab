a1832943e42c5d9540e0949e5f976687629e94c9
Alison Díaz <68522468+alisondiazc@users.noreply.github.com>
2022-09-08 01:11:42 -0500

    Update and rename 1. QC of reads to 1. QC of reads.sh

---
+++ 1. QC of reads.sh
@@ -0,0 +1,15 @@
+#Crear carpeta de reads 
+ mkdir reads 
+ cp /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYH007/Cell-1/m64140_220419_014014.hifi_reads.fastq.gz .
+ cp /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYH007/Cell-2/m64140_220420_113131.hifi_reads.fastq.gz .
+ cp /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYH007/Cell-3/m54336U_220420_105237.hifi_reads.fastq.gz .
+
+#Unir los reads en un mismo archivo 
+cat *.fastq.gz > PYM007_reads.fastq.gz 
+
+#Crear carpetas para QC
+mkdir 1.QC
+cd 1.QC 
+mkdir LongQC
+mkdir FastQC 
+
