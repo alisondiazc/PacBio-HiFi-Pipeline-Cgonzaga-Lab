6721732ce722992a6fe0f424a27215249eb17ea2
Alison Díaz <68522468+alisondiazc@users.noreply.github.com>
2022-09-12 12:24:58 -0500

    Update 1. QC of reads.sh

--- 1. QC of reads.sh
+++ 1. QC of reads.sh
@@ -32,7 +32,9 @@ mkdir 2.ReadsTrimming
 cd 2.ReadsTrimming
 ln -s /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/reads/PYM007_reads.fastq.gz .
 module load hifiadapterfilt/2.0.0
-pbadapterfilt.sh -p ./ -l 44 -m 97 -t 60 -o . 
+module load blast+/2.13.0
+module load bamtools/2.5.1
+pbadapterfilt.sh -p ./ -l 44 -m 97 -t 60 -o Hifiadapterfilt_PYM007
 
 #Ensamble de novo
 #Ensamble 
