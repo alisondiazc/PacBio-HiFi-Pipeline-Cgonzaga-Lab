8ab711e0ea2c776b207a146d5f17d1eff2e28ace
Alison Díaz <68522468+alisondiazc@users.noreply.github.com>
2022-09-12 12:27:33 -0500

    Update 1. QC of reads.sh

--- 1. QC of reads.sh
+++ 1. QC of reads.sh
@@ -34,6 +34,7 @@ ln -s /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/reads/PYM007_reads.fas
 module load hifiadapterfilt/2.0.0
 module load blast+/2.13.0
 module load bamtools/2.5.1
+#Nota: opción -p indica el prefix o ruta del archivo (mas no el archivo), es mejor dejar ./ en la carpeta donde tengamos solo el archivo
 pbadapterfilt.sh -p ./ -l 44 -m 97 -t 60 -o Hifiadapterfilt_PYM007
 
 #Ensamble de novo
