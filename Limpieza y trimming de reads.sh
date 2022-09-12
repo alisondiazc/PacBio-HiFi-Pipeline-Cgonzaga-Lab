96da307494f9d6caa92064fb69ed003b20afb689
Alison Díaz <68522468+alisondiazc@users.noreply.github.com>
2022-09-12 12:47:03 -0500

    Create Limpieza y trimming de reads.sh

---
+++ Limpieza y trimming de reads.sh
@@ -0,0 +1,9 @@
+# Limpieza y filtrado de reads 
+mkdir 2.ReadsTrimming
+cd 2.ReadsTrimming
+ln -s /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/reads/PYM007_reads.fastq.gz .
+module load hifiadapterfilt/2.0.0
+module load blast+/2.13.0
+module load bamtools/2.5.1
+## Nota: opción -p indica el prefix o ruta del archivo (mas no el archivo), es mejor dejar ./ en la carpeta donde tengamos solo el archivo
+pbadapterfilt.sh -p ./ -l 44 -m 97 -t 60 -o Hifiadapterfilt_PYM007
