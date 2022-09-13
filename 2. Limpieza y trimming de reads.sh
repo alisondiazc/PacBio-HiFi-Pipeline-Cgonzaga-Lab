cde4da201a023a27ecc393d93c06d441fa2aba1a
Alison Díaz <68522468+alisondiazc@users.noreply.github.com>
2022-09-12 21:15:54 -0500

    Update 2. Limpieza y trimming de reads.sh

--- 2. Limpieza y trimming de reads.sh
+++ 2. Limpieza y trimming de reads.sh
@@ -1,3 +1,4 @@
+
 # Limpieza y filtrado de reads 
 mkdir 2.ReadsTrimming
 cd 2.ReadsTrimming
@@ -7,3 +8,4 @@ module load blast+/2.13.0
 module load bamtools/2.5.1
 ## Nota: opción -p indica el prefix o ruta del archivo (mas no el archivo), es mejor dejar ./ en la carpeta donde tengamos solo el archivo
 pbadapterfilt.sh -p ./ -l 44 -m 97 -t 60 -o Hifiadapterfilt_PYM007
+
