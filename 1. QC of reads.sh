1c6b22ae3bd394681efcc0ff73bc671e0af29e16
Alison Díaz <68522468+alisondiazc@users.noreply.github.com>
2022-09-12 21:17:59 -0500

    Update 1. QC of reads.sh

--- 1. QC of reads.sh
+++ 1. QC of reads.sh
@@ -27,16 +27,6 @@ ln -s /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/reads/PYM007_reads.fas
 module load fastqc/0.11.3
 fastqc PYM007_reads.fastq.gz 
 
-# Limpieza y filtrado de reads 
-mkdir 2.ReadsTrimming
-cd 2.ReadsTrimming
-ln -s /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/reads/PYM007_reads.fastq.gz .
-module load hifiadapterfilt/2.0.0
-module load blast+/2.13.0
-module load bamtools/2.5.1
-## Nota: opción -p indica el prefix o ruta del archivo (mas no el archivo), es mejor dejar ./ en la carpeta donde tengamos solo el archivo
-pbadapterfilt.sh -p ./ -l 44 -m 97 -t 60 -o Hifiadapterfilt_PYM007
-
 # Ensamble de novo##
 module load hifiasm/0.16.1
 hifiasm -o PYM007_Hifiasm_denovo.asm --primary -t 8 -f 37 PYH007_reads.fastq.gz
