19ff4b23c69b4c0c8cd4ef769ddacd81c59e8c94
Alison Díaz <68522468+alisondiazc@users.noreply.github.com>
2022-09-26 12:41:50 -0500

    Update 3.DeNovoAssembly.sh

--- 3.DeNovoAssembly.sh
+++ 3.DeNovoAssembly.sh
@@ -1,1 +1,6 @@
-
+# Ensamble de novo
+mkdir 3.DeNovoAssembly
+cd 3.DeNovoAssembly
+ln -s /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/2.ReadsTrimming/Hifiadapterfilt_PYM007/PYM007_reads.filt.fastq.gz
+module load hifiasm/0.16.1
+hifiasm -o PYM007_Hifiasm_denovo.asm --primary -t 8 -f 37 PYH007_reads.fastq.gz
