e1a271ee8a91a66528acaf96b8a96279bc4176f5
Alison Díaz <68522468+alisondiazc@users.noreply.github.com>
2022-10-10 00:06:29 -0500

    Update 7.SNVcalling_T2T.sh

--- 7.SNVcalling_T2T.sh
+++ 7.SNVcalling_T2T.sh
@@ -1,22 +1,22 @@
 #Preparar el ambiente
 ##Deepvariant tiene problemas al correr con links dinámicos, por eso copiamos los archivos
-mkdir 6.SNVcalling38
-cd 6.SNVcalling38 
-cp /mnt/Timina/cgonzaga/resources/GRCh38.14/Homo_sapiens_GRCh38.p14.noMT.fasta . 
-cp /mnt/Timina/cgonzaga/resources/GRCh38.14/Homo_sapiens_GRCh38.p14.noMT.fasta.fai .
-cp /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/4.GRCh38Assembly/PYM007.GRCh38.pbmm2.bam .
-cp /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/4.GRCh38Assembly/PYM007.GRCh38.pbmm2.bam.bai . 
+mkdir 7.SNVcallingT2T
+cd 7.SNVcallingT2T
+cp /mnt/Timina/cgonzaga/resources/T2T_2.0/chm13v2.0.noMT.fa .
+cp /mnt/Timina/cgonzaga/resources/T2T_2.0/chm13v2.0.noMT.fa.fai .
+cp /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/5.T2TAssembly/PYM007.T2T.pbmm2.bam .
+cp /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/5.T2TAssembly/PYM007.T2T.pbmm2.bam.bai . 
 
 #Correr deepvariant
 module load singularity/3.7.0
 singularity run \
 -B /cm/shared/apps/singularity/images/3.7.0/deepvariant_1.4.0.sif \
 -B /usr/lib/locale/:/usr/lib/locale/ \
--B /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/6.SNVcalling38 \
+-B /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/7.SNVcallingT2T \
 docker://google/deepvariant:1.4.0 \
 /opt/deepvariant/bin/run_deepvariant \
 --model_type PACBIO \
---ref /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/6.SNVcalling38/Homo_sapiens_GRCh38.p14.noMT.fasta \
---reads /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/6.SNVcalling38/PYM007.GRCh38.pbmm2.bam \
---output_vcf /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/6.SNVcalling38/PYM007_GRCh38_deepvariant.vcf.gz \
+--ref /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/7.SNVcallingT2T/chm13v2.0.noMT.fa \
+--reads /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/7.SNVcallingT2T/PYM007.T2T.pbmm2.bam \
+--output_vcf /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/7.SNVcallingT2T/PYM007_T2T_deepvariant.vcf.gz \
 --num_shards 40
