1d5df804817d855c811035875cd26f85e8686f34
Alison Díaz <68522468+alisondiazc@users.noreply.github.com>
2022-11-15 16:07:07 -0600

    Update 6. Liftover T2T-GRCh38.sh

--- 6. Liftover T2T-GRCh38.sh
+++ 6. Liftover T2T-GRCh38.sh
@@ -1,23 +1,57 @@
-#Crear archivo chain 
-##Descargar archivo main.nf de github
-curl -o main.nf https://raw.githubusercontent.com/evotools/nf-LO/main/main.nf
-##Correr nextflow 
-module load nextflow/21.10.6
-module load minimap2/2.24
+#Crear carpeta, mover genomas, indices y bam
+mkdir 8.Liftover
+cd 8.Liftover
 ln -s /mnt/Timina/cgonzaga/resources/GRCh38.14/Homo_sapiens_GRCh38.p14.noMT.fasta .
+ln -s /mnt/Timina/cgonzaga/resources/GRCh38.14/Homo_sapiens_GRCh38.p14.noMT.fasta.fai .
 ln -s /mnt/Timina/cgonzaga/resources/T2T_2.0/chm13v2.0.noMT.fa .
-nextflow run evotools/nf-LO --source chm13v2.0.noMT.fa --target Homo_sapiens_GRCh38.p14.noMT.fasta --outdir T2T_38_liftover -profile conda --aligner minimap2/2.24
-#Si da error intentar con conda 
+ln -s /mnt/Timina/cgonzaga/resources/T2T_2.0/chm13v2.0.noMT.fa.fai .
+ln -s /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/5.T2TAssembly/PYM007.T2T.pbmm2.bam .
+ln -s /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/5.T2TAssembly/PYM007.T2T.pbmm2.bam.bai . 
+
+#Descargar archivo chain 
+wget https://hgdownload.soe.ucsc.edu/goldenPath/hs1/liftOver/chm13v2-hg38.over.chain.gz
+
+#Cambiar encabezados de GRCh38 para que coincidan con archivo chain 
+sed -i 's/CM000663.2/chr1/g' Homo_sapiens_GRCh38.p14.noMT.fasta
+sed -i 's/CM000664.2/chr2/g' Homo_sapiens_GRCh38.p14.noMT.fasta
+sed -i 's/CM000665.2/chr3/g' Homo_sapiens_GRCh38.p14.noMT.fasta
+sed -i 's/CM000666.2/chr4/g' Homo_sapiens_GRCh38.p14.noMT.fasta
+sed -i 's/CM000667.2/chr5/g' Homo_sapiens_GRCh38.p14.noMT.fasta
+sed -i 's/CM000668.2/chr6/g' Homo_sapiens_GRCh38.p14.noMT.fasta
+sed -i 's/CM000669.2/chr7/g' Homo_sapiens_GRCh38.p14.noMT.fasta
+sed -i 's/CM000670.2/chr8/g' Homo_sapiens_GRCh38.p14.noMT.fasta
+sed -i 's/CM000671.2/chr9/g' Homo_sapiens_GRCh38.p14.noMT.fasta
+sed -i 's/CM000672.2/chr10/g' Homo_sapiens_GRCh38.p14.noMT.fasta
+sed -i 's/CM000673.2/chr11/g' Homo_sapiens_GRCh38.p14.noMT.fasta
+sed -i 's/CM000674.2/chr12/g' Homo_sapiens_GRCh38.p14.noMT.fasta
+sed -i 's/CM000675.2/chr13/g' Homo_sapiens_GRCh38.p14.noMT.fasta
+sed -i 's/CM000676.2/chr14/g' Homo_sapiens_GRCh38.p14.noMT.fasta
+sed -i 's/CM000677.2/chr15/g' Homo_sapiens_GRCh38.p14.noMT.fasta
+sed -i 's/CM000678.2/chr16/g' Homo_sapiens_GRCh38.p14.noMT.fasta
+sed -i 's/CM000679.2/chr17/g' Homo_sapiens_GRCh38.p14.noMT.fasta
+sed -i 's/CM000680.2/chr18/g' Homo_sapiens_GRCh38.p14.noMT.fasta
+sed -i 's/CM000681.2/chr19/g' Homo_sapiens_GRCh38.p14.noMT.fasta
+sed -i 's/CM000682.2/chr20/g' Homo_sapiens_GRCh38.p14.noMT.fasta
+sed -i 's/CM000683.2/chr21/g' Homo_sapiens_GRCh38.p14.noMT.fasta
+sed -i 's/CM000684.2/chr22/g' Homo_sapiens_GRCh38.p14.noMT.fasta
+sed -i 's/CM000685.2/chrX/g' Homo_sapiens_GRCh38.p14.noMT.fasta
+sed -i 's/CM000686.2/chrY/g' Homo_sapiens_GRCh38.p14.noMT.fasta
 
 #Crear index 
-leviosam2 index -c source_to_target.chain -p source_to_target -F target.fai
+module load anaconda3/2021.05
+source activate leviosam2
+leviosam2 index -c chm13v2-hg38.over.chain.gz -p chm13_2.0_to_GRCh38 -F Homo_sapiens_GRCh38.p14.noMT.fasta.fai
 
+#Liftover T2T a GRCh38 
+wget https://github.com/milkschen/leviosam2/blob/main/configs/pacbio_all.yaml
+wget https://github.com/milkschen/leviosam2/blob/main/workflow/leviosam2.sh
+module load minimap2/2.24
+module load samtools/1.16.1
 bash leviosam2.sh \
-    -a minimap2 -g 1000 -H 100 -S -x ../configs/pacbio_all.yaml \
+    -a minimap2 -g 1000 -H 100 -S -x /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/8.Liftover/pacbio_all.yaml \
     -l map-hifi \
-    -i pacbio.bam \
-    -o pacbio-lifted \
-    -f grch38.fna \
-    -C chm13v2-grch38.clft \
-    -t 16 \
-    -R suppress_annotations.bed # optional
+    -i PYM007.T2T.pbmm2.bam \
+    -o PYM007.T2T.pbmm2.lifted \
+    -f Homo_sapiens_GRCh38.p14.noMT.fasta \
+    -C chm13_2.0_to_GRCh38.clft \
+    -t 20
