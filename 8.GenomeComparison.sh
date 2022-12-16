e6b1e866da64ad0888aca351adeef71c72af033d
Alison Díaz <68522468+alisondiazc@users.noreply.github.com>
2022-12-16 11:54:37 -0600

    Update 8.GenomeComparison.sh

--- 8.GenomeComparison.sh
+++ 8.GenomeComparison.sh
@@ -1,1 +1,21 @@
+#Preparar carpeta y archivos 
+mkdir 9.vsAssemblies
+cd 9.vsAssemblies 
+ln -s /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/3.DeNovoAssembly/PYM007_hifiasm_denovo.fa .
+ln -s /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/4.GRCh38Assembly/PYM007.GRCh38.pbmm2.cons.fa .
+ln -s /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/5.T2TAssembly/PYM007.T2T.pbmm2.cons.fa .
 
+#Comparar GRCh38 vs T2T
+module load gsalign/29nov22
+module load gnuplot/5.2.6
+GSAlign -r PYM007.GRCh38.pbmm2.cons.fa -q PYM007.T2T.pbmm2.cons.fa -o PYM007_GRCh38vsT2T -t 20
+
+#Comparar GRCh38 vs de novo
+module load gsalign/29nov22
+module load gnuplot/5.2.6
+GSAlign -r PYM007.GRCh38.pbmm2.cons.fa -q PYM007_hifiasm_denovo.fa -o PYM007_GRCh38vsT2T -t 20
+
+#Comparar T2T vs de novo
+module load gsalign/29nov22
+module load gnuplot/5.2.6
+GSAlign -r PYM007.T2T.pbmm2.cons.fa -q PYM007_hifiasm_denovo.fa -o PYM007_GRCh38vsT2T -t 20
