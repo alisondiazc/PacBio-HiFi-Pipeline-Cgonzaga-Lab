7dd104041209680055c97b8fd116e0cd90f424f7
Alison Díaz <68522468+alisondiazc@users.noreply.github.com>
2022-12-19 13:24:26 -0600

    Update 8.GenomeComparison.sh

--- 8.GenomeComparison.sh
+++ 8.GenomeComparison.sh
@@ -9,13 +9,30 @@ ln -s /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/5.T2TAssembly/PYM007.T
 module load gsalign/29nov22
 module load gnuplot/5.2.6
 GSAlign -r PYM007.GRCh38.pbmm2.cons.fa -q PYM007.T2T.pbmm2.cons.fa -o PYM007_GRCh38vsT2T -t 20
+module load minimap2/2.24
+minimap2 -x asm5 -L --secondary=no -t 60 PYM007.GRCh38.pbmm2.cons.fa PYM007.T2T.pbmm2.cons.fa > PYM007.GRCh38vsT2T.mm2.paf
+
+module load r/4.0.2
+./pafCoordsDotPlotly.R -i PYM007.GRCh38vsT2T.mm2.paf -o PYM007.GRCh38vsT2T -s -t -l -x
 
 #Comparar GRCh38 vs de novo
 module load gsalign/29nov22
 module load gnuplot/5.2.6
 GSAlign -r PYM007.GRCh38.pbmm2.cons.fa -q PYM007_hifiasm_denovo.fa -o PYM007_GRCh38vsDenovo -t 20
 
+module load minimap2/2.24
+minimap2 -x asm5 -L --secondary=no -t 60 PYM007.GRCh38.pbmm2.cons.fa PYM007_hifiasm_denovo.fa > PYM007.GRCh38vsDenovo.mm2.paf
+
+module load r/4.0.2
+./pafCoordsDotPlotly.R -i PYM007.GRCh38vsDenovo.mm2.paf -o PYM007.GRCh38vsDenovo -s -t -l -x
+
 #Comparar T2T vs de novo
 module load gsalign/29nov22
 module load gnuplot/5.2.6
 GSAlign -r PYM007.T2T.pbmm2.cons.fa -q PYM007_hifiasm_denovo.fa -o PYM007_T2TvsDenovo -t 20
+
+module load minimap2/2.24
+minimap2 -x asm5 -L --secondary=no -t 60 PYM007.T2T.pbmm2.cons.fa PYM007_hifiasm_denovo.fa > PYM007.T2TvsDenovo.mm2.paf
+
+module load r/4.0.2
+./pafCoordsDotPlotly.R -i PYM007.T2TvsDenovo.mm2.paf -o PYM007.T2TvsDenovo -s -t -l -x
