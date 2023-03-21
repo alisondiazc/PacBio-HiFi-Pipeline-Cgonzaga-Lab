92a90fdf53c351433393567e61e271cdd7f5fd36
Alison Díaz <68522468+alisondiazc@users.noreply.github.com>
2023-03-20 20:17:56 -0600

    Update 7.SNVcalling_T2T.sh

--- 7.SNVcalling_T2T.sh
+++ 7.SNVcalling_T2T.sh
@@ -4,20 +4,20 @@ mkdir 7.SNVcallingT2T
 cd 7.SNVcallingT2T
 cp /mnt/Timina/cgonzaga/resources/T2T_2.0/chm13v2.0.noMT.fa .
 cp /mnt/Timina/cgonzaga/resources/T2T_2.0/chm13v2.0.noMT.fa.fai .
-cp /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/5.T2TAssembly/PYM007.T2T.pbmm2.bam .
-cp /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/5.T2TAssembly/PYM007.T2T.pbmm2.bam.bai . 
+cp /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/5.T2TAssemblyNames/PYM007.T2T.pbmm2.bam .
+cp /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/5.T2TAssemblyNames/PYM007.T2T.pbmm2.bam.bai . 
 
 #Correr deepvariant
 module load singularity/3.7.0
 singularity run \
 -B /cm/shared/apps/singularity/images/3.7.0/deepvariant_1.4.0.sif \
 -B /usr/lib/locale/:/usr/lib/locale/ \
--B /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/7.SNVcallingT2T \
+-B /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/7.SNVcallingT2TNames \
 docker://google/deepvariant:1.4.0 \
 /opt/deepvariant/bin/run_deepvariant \
 --model_type PACBIO \
---ref /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/7.SNVcallingT2T/chm13v2.0.noMT.fa \
---reads /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/7.SNVcallingT2T/PYM007.T2T.pbmm2.bam \
+--ref /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/7.SNVcallingT2TNames/chm13v2.0.noMT.names.fa \
+--reads /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/7.SNVcallingT2TNames/PYM007.T2T.pbmm2.bam \
 --output_vcf /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/7.SNVcallingT2T/PYM007_T2T_deepvariant.vcf.gz \
 --num_shards 40
 
