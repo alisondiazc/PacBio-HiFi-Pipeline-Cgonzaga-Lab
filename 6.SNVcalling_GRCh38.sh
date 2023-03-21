38ab8fd03b411de430ff367e2a5abc171ea89f15
Alison Díaz <68522468+alisondiazc@users.noreply.github.com>
2023-03-20 20:21:21 -0600

    Update 6.SNVcalling_GRCh38.sh

--- 6.SNVcalling_GRCh38.sh
+++ 6.SNVcalling_GRCh38.sh
@@ -2,23 +2,23 @@
 ##Deepvariant tiene problemas al correr con links dinámicos, por eso copiamos los archivos
 mkdir 6.SNVcalling38
 cd 6.SNVcalling38 
-cp /mnt/Timina/cgonzaga/resources/GRCh38.14/Homo_sapiens_GRCh38.p14.noMT.fasta . 
-cp /mnt/Timina/cgonzaga/resources/GRCh38.14/Homo_sapiens_GRCh38.p14.noMT.fasta.fai .
-cp /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/4.GRCh38Assembly/PYM007.GRCh38.pbmm2.bam .
-cp /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/4.GRCh38Assembly/PYM007.GRCh38.pbmm2.bam.bai . 
+cp /mnt/Timina/cgonzaga/resources/GRCh38.14/Homo_sapiens_GRCh38.p14.noMT.names.fasta . 
+cp /mnt/Timina/cgonzaga/resources/GRCh38.14/Homo_sapiens_GRCh38.p14.noMT.names.fasta.fai .
+cp /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/4.GRCh38AssemblyNames/PYM007.GRCh38.pbmm2.bam .
+cp /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/4.GRCh38AssemblyNames/PYM007.GRCh38.pbmm2.bam.bai . 
 
 #Correr deepvariant
 module load singularity/3.7.0
 singularity run \
 -B /cm/shared/apps/singularity/images/3.7.0/deepvariant_1.4.0.sif \
 -B /usr/lib/locale/:/usr/lib/locale/ \
--B /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/6.SNVcalling38 \
+-B /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/6.SNVcalling38Names \
 docker://google/deepvariant:1.4.0 \
 /opt/deepvariant/bin/run_deepvariant \
 --model_type PACBIO \
---ref /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/6.SNVcalling38/Homo_sapiens_GRCh38.p14.noMT.fasta \
---reads /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/6.SNVcalling38/PYM007.GRCh38.pbmm2.bam \
---output_vcf /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/6.SNVcalling38/PYM007_GRCh38_deepvariant.vcf.gz \
+--ref /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/6.SNVcalling38Names/Homo_sapiens_GRCh38.p14.noMT.names.fasta \
+--reads /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/6.SNVcalling38Names/PYM007.GRCh38.pbmm2.bam \
+--output_vcf /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/6.SNVcalling38Names/PYM007_GRCh38_deepvariant.vcf.gz \
 --num_shards 40
 
 #Estadisticas básicas de VCF 
