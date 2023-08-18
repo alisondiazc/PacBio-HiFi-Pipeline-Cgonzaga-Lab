8434d8d22a7a39fc97d618762286a66ae8de3463
Alison Díaz <68522468+alisondiazc@users.noreply.github.com>
2023-08-17 23:49:58 -0600

    Update 8.GenomeComparison.sh

--- 8.GenomeComparison.sh
+++ 8.GenomeComparison.sh
@@ -5,6 +5,8 @@ ln -s /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/3.DeNovoAssembly/PYM00
 ln -s /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/4.GRCh38Assembly/PYM007.GRCh38.pbmm2.cons.fa .
 ln -s /mnt/Timina/cgonzaga/adiaz/PacBio_secuencias/PYM007/5.T2TAssembly/PYM007.T2T.pbmm2.cons.fa .
 
+GSAlign -r PYM007.GRCh38.pbmm2.cons.fa -q PYM007.T2T.pbmm2.cons.fa -o GS/PYM007_GRCh38vsT2T -t 20
+
 #Comparar GRCh38 vs T2T
 module load gsalign/29nov22
 module load gnuplot/5.2.6
